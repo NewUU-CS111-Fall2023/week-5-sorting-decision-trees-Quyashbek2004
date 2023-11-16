84a129c1dc84f8d7d80c76422cdb1353644a78c3
Quyashbek2004 <127374399+Quyashbek2004@users.noreply.github.com>
2023-11-16 16:52:27 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,35 @@
  * Date:
  * Name:
  */
+#include <iostream>
+#include <vector>
+#include <string>
+
+void bubbleSort(std::vector<std::string>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<std::string> books = {"Harry Potter", "To Kill a Mockingbird", "The Great Gatsby", "1984", "Brave New World"};
+    
+    std::cout << "Unsorted list of books:" << std::endl;
+    for (const auto& book : books) {
+        std::cout << book << std::endl;
+    }
+    
+    bubbleSort(books);
+    
+    std::cout << "\nSorted list of books:" << std::endl;
+    for (const auto& book : books) {
+        std::cout << book << std::endl;
+    }
+    
+    return 0;
+}
